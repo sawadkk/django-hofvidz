a187107c9f5f4d66d36c0592f81bef79a5e45a73
sawad_kk <ssawad09@gmail.com>
2021-05-24 17:34:43 +0530

    authentication

--- halls/views.py
+++ halls/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
 	return render(request, "halls/home.html")
+ 
+class SignUp(generic.CreateView):
+ 	form_class = UserCreationForm
+ 	success_url = reverse_lazy("home")
+ 	template_name = 'registration/signup.html'	
--- hofvidz/urls.py
+++ hofvidz/urls.py
@@ -13,7 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin,auth
+from django.contrib.auth import views as auth_views 
 from django.urls import path,include
 from halls import views
 from django.conf.urls.static import static
@@ -21,6 +22,9 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",views.home,name="home")
+    path("",views.home,name="home"),
+    path('signup',views.SignUp.as_view(),name="signup"),
+    path('login',auth.views.LoginView.as_view(),name="login"),
+    path('logout',auth.views.LogoutView.as_view(),name="logout"),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
